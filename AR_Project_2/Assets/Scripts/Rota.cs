d8fb246f65482b4fcbb7ee9e579759bd480d4040
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2017-08-01 13:28:42 -0500

    Poniendo botón para rotar objeto 3D

---
+++ AR_Project_2/Assets/Scripts/Rota.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Importar libreria de Vuforia
+using Vuforia;
+
+//Heredar clase IVirtualButtonEventHandler
+public class Rota : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    private GameObject VBButtonObject;
+    private GameObject zombie;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Unir con el botón de vuforia
+        VBButtonObject = GameObject.Find("RotateZombieButton");
+        VBButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        zombie = GameObject.Find("zombie");
+    }
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        zombie.transform.Rotate(new Vector3(0, Time.deltaTime * 1000, 0));
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("Epa");
+    }
+}
